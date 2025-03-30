840ae960c66760dc822357e58dd9dc4cda2c0838
RaveeshaPeiris <rpeiris4444@gmail.com>
2025-03-31 04:13:30 +0530

    collection page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
-createRoot(document.getElementById('root')).render(
+//createRoot(document.getElementById('root')).render(
+ReactDom.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+  <ShopContextProvider>
+  <App />
+  </ShopContextProvider>
+   
   </BrowserRouter>,
 )
